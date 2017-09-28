422e4b741754976f8a70ab1e86fc1bda706d871c
shangguanMr <914132996@qq.com>
2017-09-28 11:42:48 +0800

    配置

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class index extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    componentWillmount(){
+
+    }
+    componentDidmount(){
+
+    }
+    render(){}
+
+}
+
+export default index;
